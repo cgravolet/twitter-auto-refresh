b4650c404fb6b37c4ec2d6a5940b06bce6844003
Charles G <cgravolet@gmail.com>
2015-03-14 14:11:34 -0400

    Initial commit 2

---
+++ content-script.js
@@ -0,0 +1,104 @@
+/**
+ * Twitter Refresh
+ * @author Charles Gravolet
+ * @version 1.0
+ */
+var TwitterRefresh = {
+
+	init: function () {
+		var self = this;
+
+		function getNewItemsBar() {
+			var newItemsBar = document.getElementsByClassName("js-new-items-bar-container");
+
+			if (newItemsBar.length) {
+				document.removeEventListener("DOMSubtreeModified", getNewItemsBar);
+				self.setNewItemsBar(newItemsBar[0]);
+			}
+		}
+
+		// Listen for changes to the document in order to find the new-items bar
+		document.addEventListener("DOMSubtreeModified", getNewItemsBar);
+
+		return this;
+	},
+
+	addToggleButton: function() {
+		var globalActions = document.getElementById("global-actions");
+
+		if (globalActions) {
+			var li   = document.createElement("li");
+			var a    = document.createElement("a");
+			var icon = document.createElement("span");
+			var text = document.createElement("span");
+
+			this.refreshIcon = icon;
+			this.setColorOfRefreshIcon();
+
+			// Setup elements
+			icon.className = "Icon Icon--refresh Icon--large";
+			text.className = "text";
+			a.className    = "js-tooltip js-dynamic-tooltip";
+			a.setAttribute("href", "#");
+			a.setAttribute("data-nav", "auto-refresh");
+			a.setAttribute("data-placement", "bottom");
+			a.addEventListener("click", this.toggleAutoRefresh.bind(this));
+
+			// Append elements to the document
+			text.appendChild(document.createTextNode("Auto-Refresh"));
+			a.appendChild(icon);
+			a.appendChild(text);
+			li.appendChild(a);
+			globalActions.appendChild(li);
+		}
+	},
+
+	enabled: function (setting) {
+
+		if (setting === true) {
+			localStorage.setItem("twitterAutoRefresh", "true");
+			this.refreshTimeline();
+			return true;
+		} else if (setting === false) {
+			localStorage.setItem("twitterAutoRefresh", "false");
+			return false;
+		}
+
+		return localStorage.getItem("twitterAutoRefresh") === "false" ? false : true;
+	},
+
+	refreshTimeline: function () {
+		var newTweets = this.newItemsBar.getElementsByClassName("js-new-tweets-bar");
+
+		if (newTweets.length && this.enabled()) {
+			newTweets[0].click();
+		}
+	},
+
+	setColorOfRefreshIcon: function () {
+
+		if (this.enabled()) {
+			this.refreshIcon.style.color = "";
+		} else {
+			this.refreshIcon.style.color = "#CCCCCC";
+		}
+	},
+
+	setNewItemsBar: function (newItemsBar) {
+		this.newItemsBar = newItemsBar;
+
+		// Add listener for new items bar
+		this.newItemsBar.addEventListener("DOMSubtreeModified", this.refreshTimeline.bind(this));
+
+		// Add toggle switch to the twitter header
+		this.addToggleButton();
+	},
+
+	toggleAutoRefresh: function (e) {
+		e.preventDefault();
+		this.enabled(this.enabled() ? false : true);
+		this.setColorOfRefreshIcon();
+	}
+};
+
+var twitterRefresh = Object.create(TwitterRefresh).init();
